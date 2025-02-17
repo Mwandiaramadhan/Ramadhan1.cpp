815b6cabc5f676e96a4fa23c1bdf4d4869f8650f
Mwandiaramadhan <ramadhanmwandia@gmail.com>
2025-02-17 09:19:59 +0300

    Add files via upload

---
+++ Ramadhan1.cpp
@@ -0,0 +1,30 @@
+/*
+Author: Ramadhan Mwandia
+Reg no BSE-05-0201/2024
+*/
+#include <iostream>
+#include <cmath> // For M_PI
+using namespace std;
+
+double calculateCylinderVolume(double radius, double height) {
+    // Calculate the volume of the cylinder
+    double volume = M_PI * pow(radius, 2) * height;
+    return volume;
+}
+
+int main() {
+    double radius, height;
+
+    // Prompt the user for input
+    cout << "Enter the radius of the cylinder: ";
+    cin >> radius;
+
+    cout << "Enter the height of the cylinder: ";
+    cin >> height;
+
+    // Calculate and display the volume
+    double volume = calculateCylinderVolume(radius, height);
+    cout << "The volume of the cylinder is: " << volume << endl;
+
+    return 0;
+}
